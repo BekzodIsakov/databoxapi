5bbb87abc4739f31a2beff5b0e530b2e418efb9b
Bekzod <bekzodisakov18@gmail.com>
2024-10-25 18:52:09 +0500

    add new route to create new product

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const router = new express.Router();
 const { ProductModel } = require("../models");
 
@@ -6,6 +7,17 @@ router.get("/", async (req, res, next) => {
   try {
     const products = await ProductModel.find();
     res.send(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Post request doesn't in fact create anything, it just an immmitation
+router.post("/", async (req, res, next) => {
+  try {
+    const product = new ProductModel(req.body);
+    await product.validate();
+    res.status(201).send(product);
   } catch (error) {
     next(error);
   }
