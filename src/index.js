5bce66ebc7701e12429bf83a4a904c7f6ba3df1b
Bekzod <bekzodisakov18@gmail.com>
2024-11-03 17:29:15 +0500

    install daisyUI

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const hostname = process.env.HOSTNAME || "localhost";
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 // app.use("views", viewsDirPath);
